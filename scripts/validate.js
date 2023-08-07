7c41e2ee38a5b92a4adc698c802295f7509a2b1f
Alexander Kolchuk <forestrun342@gmail.com>
2023-08-07 18:31:00 +0300

    feat: add validation form

---
+++ scripts/validate.js
@@ -0,0 +1,70 @@
+const classListForm = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'popup__submit_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input_type_visible',
+};
+
+const showValidationError = (formItem, inputItem, errorMessage, settings) => {
+  const errorItem = formItem.querySelector(`.${inputItem.id}-error`);
+  inputItem.classList.add(settings.inputErrorClass);
+  errorItem.textContent = errorMessage;
+  errorItem.classList.add(settings.errorClass);
+};
+
+const hideValidationError = (formItem, inputItem, settings) => {
+  const errorItem = formItem.querySelector(`.${inputItem.id}-error`);
+  inputItem.classList.remove(settings.inputErrorClass);
+  errorItem.classList.remove(settings.errorClass);
+  errorItem.textContent = '';
+};
+
+const checkInputValidity = (formItem, inputItem, settings) => {
+  if(inputItem.validity.valid === false){
+    showValidationError(formItem, inputItem, inputItem.validationMessage, settings);
+  } else{
+    hideValidationError(formItem, inputItem, settings);
+  }
+};
+
+const hasInvalidInput = inputList => {
+  return inputList.some(item => {
+    return !item.validity.valid;
+  });
+};
+
+const toggleButtonState = (formItem, buttonItem, settings) => {
+  const inputList = Array.from(formItem.querySelectorAll(settings.inputSelector));
+  if(hasInvalidInput(inputList)){
+    buttonItem.classList.add(settings.inactiveButtonClass);
+    buttonItem.setAttribute('disabled', true);
+  }else{
+    buttonItem.classList.remove(settings.inactiveButtonClass);
+    buttonItem.removeAttribute('disabled');
+  }
+}
+
+const setEventListeners = (formItem, settings) => {
+  const inputList = Array.from(formItem.querySelectorAll(settings.inputSelector));
+  const buttonItem = formItem.querySelector(settings.submitButtonSelector);
+  toggleButtonState(formItem, buttonItem, settings);
+  inputList.forEach(inputItem => {
+    inputItem.addEventListener('input', () => {
+      checkInputValidity(formItem, inputItem, settings);
+      toggleButtonState(formItem, buttonItem, settings);
+    });
+  });
+}
+
+const enableValidation = (settings) => {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach(formItem => {
+    setEventListeners(formItem, settings);
+  });
+}
+
+enableValidation(classListForm);
+
+
